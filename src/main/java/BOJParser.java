5185cdcfa14b348b4b05b4c892e7aa548bd195e1
YoonSubLim <yslim37@naver.com>
2024-04-30 10:43:12 +0900

    feat: enable to multiple results

--- src/main/java/BOJParser.java
+++ src/main/java/BOJParser.java
@@ -3,6 +3,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -13,28 +15,31 @@ public class BOJParser {
     public static void main(String[] args) {
 
         String url = "https://www.acmicpc.net/status?option-status-pid=on&problem_id=&user_id=yslim37&language_id=-1&result_id=-1"; // 가져올 웹 페이지의 URL
+        ArrayList<String[]> solvedProblems = new ArrayList<>();
+
         try {
             // Jsoup을 사용하여 GET 요청을 보내고 HTML 문서를 파싱합니다.
             Document doc = Jsoup.connect(url).get();
 
-            // 원하는 HTML 태그를 선택합니다.
-            Elements problems = doc.select("#status-table>tbody>tr");
+            //　제출　단위로 Elements 파싱
+            Elements submits = doc.select("#status-table>tbody>tr");
 
             // 선택된 태그에서 원하는 정보를 추출합니다.
-            for (Element problem : problems) {
-                String result = problem.getElementsByClass("result-text").get(0).text();
+            for (Element submit : submits) {
+                // 결과 text
+                String result = submit.getElementsByClass("result-text").get(0).text();
                 if (result.equals("틀렸습니다")) {
                     continue;
                 }
 
-                Elements titleElement = problem.getElementsByClass("problem_title");
-                Elements dateElement = problem.getElementsByClass("show-date");
+                // 문제 정보 (제목/번호)
+                Elements titleElement = submit.getElementsByClass("problem_title");
+                String problem_title = titleElement.get(0).attr("title");
+                String problem_number = titleElement.get(0).text();
 
-                String title = titleElement.get(0).attr("title");
-                System.out.println(title);
-
+                // 제출 시간
+                Elements dateElement = submit.getElementsByClass("show-date");
                 String datetimeStr = dateElement.get(0).attr("title");
-                System.out.println(datetimeStr);
 
                 // datetime
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
@@ -49,20 +54,20 @@ public class BOJParser {
                 // 날짜 부분만 비교하여 일수 차이 계산
                 long daysDifference = ChronoUnit.DAYS.between(date1, date2);
 
-                // 현재 날짜와 비교
-                if (daysDifference == 0) {
-                    System.out.println("오늘도 풀었네");
-                    continue;
-                } else if (daysDifference == 1) {
-                    System.out.println("어제 풀었네");
-                } else {
-                    System.out.println("안풀었네. 커피사야겠다");
+                // 1일 전 푼 문제가 있다면, List 에 저장
+                if (daysDifference == 1) {
+                    // TODO : 난이도 추가 저장
+                    solvedProblems.add(new String[] {problem_title, problem_number});
+                } else if (daysDifference >= 2) {
+                    break;
                 }
-
-                break;
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        for (String[] str: solvedProblems) {
+            System.out.println(str[0] + " " + str[1]);
         }
     }
 }
